e6dde047839d0f61b5b08f2b949054f6e7702266
MolVlad <molodcov.vladik@gmail.com>
2018-09-19 21:51:38 +0300

    done

--- split.c
+++ split.c
@@ -20,10 +20,10 @@
 
 /* Defgroup for sizes ------------------------------------------------------------*/
 
-#define MAX_SIZE_TEXT 10000
-#define MAX_SIZE_DELIMITERS 10
-#define MAX_NUM_WORDS 20
-#define MAX_SIZE_WORD 100
+#define MAX_SIZE_TEXT		10000
+#define MAX_SIZE_DELIMITERS	10
+#define MAX_NUM_WORDS		20
+#define MAX_SIZE_WORD		100
 
 /* Structure for SPLIT -----------------------------------------------------------*/
 
@@ -59,10 +59,10 @@ int SPLIT_Free(SPLIT_STRUCT * split);
 int main()
 {
 	SPLIT_STRUCT split;
-	split.max_size_text = MAX_SIZE_TEXT;
-	split.max_size_delimiters = MAX_SIZE_DELIMITERS;
-	split.max_num_words = MAX_NUM_WORDS;
-	split.max_size_word = MAX_SIZE_WORD;
+	split.max_size_text = 		MAX_SIZE_TEXT;
+	split.max_size_delimiters = 	MAX_SIZE_DELIMITERS;
+	split.max_num_words = 		MAX_NUM_WORDS;
+	split.max_size_word = 		MAX_SIZE_WORD;
 
 	assert(!SPLIT_Init(&split));
 	assert(!SPLIT_Scan(&split));
@@ -78,23 +78,18 @@ int main()
 char * Allocate_memory(int num_symbols)
 {
 	char * ret = (char *)malloc((num_symbols + 1) * sizeof(char));
-	assert(ret);
 
 	return ret;
 }
 
 char ** Allocate_memory_array(int array_size, int word_size)
 {
+	char ** ret = (char **)malloc(array_size * sizeof(char *));
+
 	int i;
 
-	char ** ret = (char **)malloc(array_size * sizeof(char *));
-	assert(ret);
-
 	for(i = 0; i < array_size; i++)
-	{
 		ret[i] = (char *)malloc((word_size + 1) * sizeof(char));
-		assert(ret[i]);
-	}
 
 	return ret;
 }
@@ -113,13 +108,21 @@ void Free_memory_array(char ** array, int array_size)
 
 int SPLIT_Init(SPLIT_STRUCT * split)
 {
-	split->text = Allocate_memory(split->max_size_text);
-	split->delimiters = Allocate_memory(split->max_size_delimiters);
-	split->words = Allocate_memory_array(split->max_num_words, split->max_size_word);
-	split->count = 0;
+	split->text =		Allocate_memory(split->max_size_text);
+	split->delimiters =	Allocate_memory(split->max_size_delimiters);
+	split->words =		Allocate_memory_array(split->max_num_words, split->max_size_word);
+	split->count =		0;
 
-	if(!(split->text && split->delimiters && split->words))
+	if(!split->text || !split->delimiters || !split->words)
 		return 1;
+
+	int i;
+
+	for(i = 0; i < split->max_num_words; i++)
+	{
+		if(!split->words[i])
+			return 1;
+	}
 
 	return 0;
 }
@@ -140,12 +143,16 @@ int SPLIT_Split(SPLIT_STRUCT * split)
 	char * fragment = Allocate_memory(MAX_SIZE_WORD);
 
 	int i = 0;
+
 	fragment = strtok(split->text, split->delimiters);
 	while(fragment)
 	{
+		if(!split->words[i])
+			return 1;
+
 		strcpy(split->words[i], fragment);
-		i++;
 		fragment = strtok(NULL, split->delimiters);
+		i++;
 	}
 
 	split->count = i;
@@ -166,6 +173,7 @@ int SPLIT_Print(SPLIT_STRUCT * split)
 	{
 		if(!split->words[i])
 			return 1;
+
 		printf("%s\n", split->words[i]);
 	}
 
