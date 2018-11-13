d146773b659316690b677ab5b65a60354e08f9e9
MolVlad <molodcov.vladik@gmail.com>
2018-11-13 11:22:37 +0300

    better main

--- homework/main.c
+++ homework/main.c
@@ -7,16 +7,78 @@
 #define AMOUNT_OF_NUMBERS 100000000
 #define MAX_RANDOM 1000
 
-struct Segment
+typedef struct Segment
 {
 	int firstElement;
 	int lastElement;
-};
+} Segment;
 
 float average = 0, dispersion = 0;
 int completedThreads = 0;
 int isAverageReady = 0;
 int * array, * sum, *deviations;
+int step;
+
+void joinThreads(int numberOfThreads, pthread_t *thids);
+void *thread(void * arg);
+int allocateMemory(pthread_t ** thids, Segment ** segment, int numberOfThreads);
+void freeMemory(pthread_t * thids, Segment * segment);
+int scanNumberOfThreads();
+void generateData();
+int startThreads(int numberOfThreads, pthread_t * thids, Segment * segment);
+
+int main()
+{
+	int numberOfThreads = scanNumberOfThreads();
+	step = AMOUNT_OF_NUMBERS / numberOfThreads;
+
+	struct Segment * segment;
+	pthread_t * thids;
+	if(!allocateMemory(&thids, &segment, numberOfThreads))
+	{
+		printf("Allocate error\n");
+		exit(-1);
+	}
+
+	generateData();
+
+	double begin = clock();
+	int i, result;
+
+	startThreads(numberOfThreads, thids, segment);
+
+	while(completedThreads != numberOfThreads);
+
+	average /= AMOUNT_OF_NUMBERS;
+	isAverageReady = 1;
+
+	joinThreads(numberOfThreads, thids);
+	freeMemory(thids, segment);
+
+	dispersion /= AMOUNT_OF_NUMBERS;
+	printf("average = %f, dispersion = %f\n", average, dispersion);
+
+	double end = clock();
+	printf("time: %f\n", (end - begin) / CLOCKS_PER_SEC);
+
+	return 0;
+}
+
+void joinThreads(int numberOfThreads, pthread_t *thids)
+{
+	int i, result;
+
+	for(i = 0; i < numberOfThreads; i++)
+	{
+		result = pthread_join(thids[i], (void **)NULL);
+
+		if(result != 0)
+		{
+			printf("Error on thread join, return value = %d\n", result);
+			exit(-1);
+		}
+	}
+}
 
 void *thread(void * arg)
 {
@@ -41,30 +103,46 @@ void *thread(void * arg)
 	pthread_exit(0);
 }
 
-int main()
+int allocateMemory(pthread_t ** thids, Segment ** segment, int numberOfThreads)
+{
+	array = calloc(AMOUNT_OF_NUMBERS, sizeof(int));
+	*thids = calloc(numberOfThreads, sizeof(pthread_t));
+	*segment = (struct Segment *)calloc(numberOfThreads, sizeof(struct Segment));
+
+	return segment && thids && array;
+}
+
+void freeMemory(pthread_t * thids, Segment * segment)
+{
+	free(thids);
+	free(array);
+	free(segment);
+}
+
+int scanNumberOfThreads()
+{
+	int numberOfThreads;
+
+	printf("Enter number of threads\n");
+	scanf("%d", &numberOfThreads);
+
+	return numberOfThreads;
+}
+
+void generateData()
 {
 	srand(time(NULL));
-
-	array = calloc(AMOUNT_OF_NUMBERS, sizeof(int));
 
 	int i;
 	for(i = 0; i < AMOUNT_OF_NUMBERS; i++)
 		array[i] = rand() % MAX_RANDOM;
 
 	printf("generated succesfull\n");
+}
 
-	int numberOfThreads;
-	printf("Enter number of threads\n");
-	scanf("%d", &numberOfThreads);
-
-	int step = AMOUNT_OF_NUMBERS / numberOfThreads;
-
-	double begin = clock();
-
-	int result;
-	pthread_t * thids = malloc(numberOfThreads * sizeof(pthread_t));
-
-	struct Segment * segment = (struct Segment *)malloc(numberOfThreads * sizeof(struct Segment));
+int startThreads(int numberOfThreads, pthread_t * thids, Segment * segment)
+{
+	int i, result;
 	for(i = 0; i < numberOfThreads; i++)
 	{
 		segment[i].firstElement = i * step;
@@ -73,26 +151,11 @@ int main()
 		result = pthread_create(&thids[i], NULL, thread, &segment[i]);
 		if(result != 0)
 		{
-			printf("Error on thread create, return value = %d\n", result);
-			exit(-1);
+			return -1;
 		}
 	}
-
-	while(completedThreads != numberOfThreads);
-
-	average /= AMOUNT_OF_NUMBERS;
-	isAverageReady = 1;
-
-	for(i = 0; i < numberOfThreads; i++)
-	{
-		result = pthread_join(thids[i], (void **)NULL);
-
-		if(result != 0)
-		{
-			printf("Error on thread join, return value = %d\n", result);
-			exit(-1);
-		}
-	}
+<<<<<<< HEAD
+=======
 
 	dispersion /= AMOUNT_OF_NUMBERS;
 
@@ -109,4 +172,5 @@ int main()
 	printf("time: %f\n", (end - begin) / CLOCKS_PER_SEC);
 
 	return 0;
+>>>>>>> c66627d7c8f1663fa92e395ab50008fe89ff5c0f
 }
